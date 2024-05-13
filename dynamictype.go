7a795cb0689c331f9e989653532b3a1afa9045de
ParanoidLA <paranoidla04@gmail.com>
2024-05-13 17:03:36 +0530

    Third

---
+++ dynamictype.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+	var x float64 =20.0
+	y :=42
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("Value of x is %f\n",x)
+	fmt.Printf("Value of y is %d\n",y)
+
+}
---
+++ garbage.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)//empty
+	fmt.Println(b)//0
+	fmt.Println(c)//false
+	
+}
